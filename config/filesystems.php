110c18414117a732883c40ec02f0cad42b87f6b2
cristianbiancalana <criseze156@gmail.com>
2024-05-23 00:19:16 -0300

    fixed problem about attachment file

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,7 +36,6 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'throw' => false,
         ],
 
         'public' => [
@@ -44,7 +43,6 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
